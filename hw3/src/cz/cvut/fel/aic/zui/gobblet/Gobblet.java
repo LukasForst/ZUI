1b966eaad01d2b13333694c403dbd6c3cf52c02e
Lukas Forst <lukas.forst@blindspot.ai>
2018-04-14 15:40:09 +0200

    negascout

--- hw3/src/cz/cvut/fel/aic/zui/gobblet/Gobblet.java
+++ hw3/src/cz/cvut/fel/aic/zui/gobblet/Gobblet.java
@@ -20,25 +20,25 @@ public class Gobblet {
         //small
         //Value : -329
         // about 19000 nodes
-        int seed = 0;
-        int randomMoves = 12;
-        int depth = 3;
+//        int seed = 0;
+//        int randomMoves = 12;
+//        int depth = 3;
 
         //bigger
         //Value : 659
         //about 4 700 000 nodes
-/*      int seed = 10;
-        int randomMoves = 10;
-        int depth = 5;
-*/
+//        int seed = 10;
+//        int randomMoves = 10;
+//        int depth = 5;
+
 
         //even bigger
         //Value : -41
         //about 19 000 000 nodes
-/*      int seed = 20;
+        int seed = 20;
         int randomMoves = 12;
         int depth = 5;
-*/
+
         Board game = new Board();
         int playerToMove = randomPlay(game, randomMoves, seed);
 
--- hw3/src/cz/cvut/student/AlphaBeta.java
+++ hw3/src/cz/cvut/student/AlphaBeta.java
@@ -9,24 +9,36 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class AlphaBeta extends Algorithm {
+
+    //alpha - best known value for MAX phase
+    //beta - best known value for MIN phase
     @Override
     protected int runImplementation(Board game, int depth, int player, int alpha, int beta) {
 
         ArrayList<Move> successors = game.generatePossibleMoves(player);
 
-        Random rnd = new Random();
-        Move randomMove = successors.get(rnd.nextInt(successors.size()));
-
-        Board subGame = new Board(game);
-        if (!subGame.makeMove(randomMove))
-            System.err.println("Something is terribly wrong in AB!");
-
-        int subGameHeuristicValue = subGame.evaluateBoard();
-
-        if (depth > 0) {
-            int subGameValue = run(subGame, depth - 1, Gobblet.switchPlayer(player), alpha, beta);
+        //negascout
+        if (successors.isEmpty() || depth == 0) {
+            return game.evaluateBoard();
         }
 
-        return -1;
+        int b = beta;
+        for (int i = 0; i < successors.size(); i++) {
+            Move succ = successors.get(i);
+            Board nextGame = new Board(game);
+            nextGame.makeMove(succ);
+
+            int v = -run(nextGame, depth - 1, Gobblet.switchPlayer(player), -b, -alpha);
+            if (alpha < v && v < beta && i != 0) {
+                v = -run(nextGame, depth - 1, Gobblet.switchPlayer(player), -beta, -v);
+            }
+            alpha = Math.max(alpha, v);
+            if (alpha >= beta) {
+                return alpha;
+            }
+            b = alpha + 1;
+        }
+
+        return alpha;
     }
 }
