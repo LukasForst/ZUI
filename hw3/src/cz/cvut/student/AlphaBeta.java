a5de144b1269413c6ffb9324eb01e616512475c7
Lukas Forst <lukas.forst@blindspot.ai>
2018-04-14 17:36:37 +0200

    working alpha beta

--- hw3/src/cz/cvut/student/AlphaBeta.java
+++ hw3/src/cz/cvut/student/AlphaBeta.java
@@ -1,48 +1,68 @@
 package cz.cvut.student;
 
+import com.sun.org.apache.xml.internal.security.algorithms.implementations.IntegrityHmac;
 import cz.cvut.fel.aic.zui.gobblet.Gobblet;
 import cz.cvut.fel.aic.zui.gobblet.algorithm.Algorithm;
 import cz.cvut.fel.aic.zui.gobblet.environment.Board;
 import cz.cvut.fel.aic.zui.gobblet.environment.Move;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class AlphaBeta extends Algorithm {
+    private static final int WHITE_PLAYER = 0;
+    private static final int BLACK = 4;
+
     private int winningPlayer = Integer.MIN_VALUE;
 
     //alpha - best known value for MAX phase
     //beta - best known value for MIN phase
     @Override
     protected int runImplementation(Board game, int depth, int player, int alpha, int beta) {
-        ArrayList<Move> successors = game.generatePossibleMoves(player);
-        if (successors.isEmpty() || depth == 0) return game.evaluateBoard();
+        if(alpha == Integer.MIN_VALUE){
+            alpha -= 10;
+        }
 
-        if (winningPlayer == Integer.MIN_VALUE) {
+        if(winningPlayer == Integer.MIN_VALUE){
             winningPlayer = player;
         }
 
-        if (winningPlayer == player) {
-            int score = Integer.MIN_VALUE;
-            for (Move m : successors) {
-                Board b = new Board(game);
-                b.makeMove(m);
-                score = Math.max(score, run(b, depth - 1, Gobblet.switchPlayer(player), alpha, beta));
-                alpha = Math.max(alpha, score);
-                if (alpha >= beta) break;
-            }
-            return score;
-        } else {
-            int score = Integer.MAX_VALUE;
+        Queue<Board> queue = evaluateSuccessors(game, player);
+        if (queue.isEmpty() || depth == 0) return game.evaluateBoard();
 
-            for (Move m : successors) {
-                Board b = new Board(game);
-                b.makeMove(m);
-                score = Math.min(score, run(b, depth - 1, Gobblet.switchPlayer(player), alpha, beta));
-                beta = Math.min(beta, score);
-                if (alpha >= beta) break;
+        int b = beta;
+        int score = Integer.MIN_VALUE;
+        boolean firstChild = true;
+        while (!queue.isEmpty()){
+            Board g = queue.poll();
+            score = Math.max(score, -run(g, depth - 1, Gobblet.switchPlayer(player), -b, -alpha));
+
+            if(alpha < score){
+                score = Math.max(score, -run(g, depth - 1, Gobblet.switchPlayer(player), -beta, -alpha));
             }
-            return score;
+
+            alpha = Math.max(alpha, score);
+
+            if (beta <= alpha) break;
+            b = alpha + 1;
         }
+        return score;
+    }
+
+    private Queue<Board> evaluateSuccessors(Board game, int player){
+        Queue<Board> queue;
+
+        if(WHITE_PLAYER == winningPlayer){
+            queue = new PriorityQueue<>((o1, o2) -> o2.evaluateBoard() - o1.evaluateBoard());
+        } else {
+            queue = new PriorityQueue<>((Comparator.comparingInt(Board::evaluateBoard)));
+        }
+
+        ArrayList<Move> successors = game.generatePossibleMoves(player);
+        for(Move m : successors){
+            Board g = new Board(game);
+            g.makeMove(m);
+            queue.add(g);
+        }
+        return queue;
     }
 }
